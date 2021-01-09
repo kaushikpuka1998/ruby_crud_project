65bfd8f8eac9ee456aca8b83442ebc2fec53c9d8
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2021-01-10 00:39:51 +0530

    create subjects controller along with 5 views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'subjects/new'
+  get 'subjects/index'
+  get 'subjects/edit'
+  get 'subjects/update'
+  get 'subjects/delete'
   root 'demo#hello'
 
   get 'demo/index',to: 'demo#index', as: 'index'
